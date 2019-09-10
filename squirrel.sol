d05a8d3abf9cf02e00827dfa796fb7c6229052ec
Muhammed Nagy <me@muhnagy.com>
2019-09-10 12:18:32 +0300

    Get count of goals

--- squirrel.sol
+++ squirrel.sol
@@ -9,8 +9,11 @@ contract squirrel {
     struct goal {uint256 target ; uint256 balance;}
     
     mapping (address => goal)  goals;
-
+    uint256 count = 0;
     function createGoal(uint256 target) payable public returns (bool) {
+        if (goals[msg.sender].target == 0) {
+            count += 1;
+        }
         goals[msg.sender].target = target;
         goals[msg.sender].balance += msg.value;
         return true;
@@ -22,10 +25,14 @@ contract squirrel {
                 msg.sender.transfer(goals[msg.sender].balance);
                 goals[msg.sender].balance = 0;
                 goals[msg.sender].target = 0;
+                count -= 1;
         }
         return goals[msg.sender].balance;
     } 
     
+    function getGoalsCount() public view returns (uint256) {
+        return count;
+    }
     function getInfo() public view returns (uint256, uint256){
         return (goals[msg.sender].target, goals[msg.sender].balance); 
     }
